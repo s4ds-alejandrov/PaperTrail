f07f115c9356f1a135f56e79a21cdde9ceda765f
alejandrov <alejandrov@s4ds.com>
2014-06-05 16:24:49 -0500

    up4
    test

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,4 @@
+
 #install joomla
 bash "install_Joomla" do
   user "root"
@@ -13,16 +14,10 @@ end
 
 ruby_block "edit rsyslog.conf" do
   block do
-    rc = Chef::Util::FileEdit.new("/etc/rsyslog.conf")
-    rc.insert_line_if_no_match(/# of the default configuration above./,"*.*	@logs.papertrailapp.com:39160")
-    rc.write_file
-  end
-end
-ruby_block "edit rsyslog.conf" do
-  block do
-    rd = Chef::Util::FileEdit.new("/etc/rsyslog.conf")
-    rd.insert_line_if_no_match(/# of the default configuration above./,"local0.*		@logs.papertrailapp.com:39160")
-    rd.write_file
+    bc = Chef::Util::FileEdit.new("/etc/rsyslog.conf")
+    bc.insert_line_if_no_match(/# of the default configuration above/,"*.* @logs.papertrailapp.com:39160")
+    bc.insert_line_if_no_match(/# something./,"local0.*   @logs.papertrailapp.com:39160")
+    bc.write_file
   end
 end
 
@@ -34,13 +29,13 @@ ruby_block "edit home s4ds rsyslog.conf" do
   end
 end
 
-
 bash "insert_line" do
   user "root"
   code <<-EOS
   echo "CustomLog '|/usr/bin/logger -p local1.info -t HTTP'" >> /etc/httpd/sites-available/default
   EOS
 end
+
 #reiniciar servicio
 service "rsyslog" do
   action :restart
