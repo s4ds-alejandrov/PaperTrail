06a1dca86471a7fb301a29862a7f1933455c1e26
alejandrov <alejandrov@s4ds.com>
2014-06-05 10:28:13 -0500

    up2

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,4 @@
-
+#install joomla
 bash "install_Joomla" do
   user "root"
   cwd "/tmp"
@@ -9,23 +9,40 @@ bash "install_Joomla" do
   EOH
 end
 
+#edicion Archivos para papertrail
 
-
-ruby_block "edit home s4ds rsyslog.conf" do
+ruby_block "edit rsyslog.conf" do
   block do
     rc = Chef::Util::FileEdit.new("/etc/rsyslog.conf")
     rc.insert_line_if_no_match(/# of the default configuration above./,"*.*	@logs.papertrailapp.com:39160")
     rc.write_file
   end
 end
-
+ruby_block "edit rsyslog.conf" do
+  block do
+    rd = Chef::Util::FileEdit.new("/etc/rsyslog.conf")
+    rd.insert_line_if_no_match(/# of the default configuration above./,"local0.*		@logs.papertrailapp.com:39160")
+    rd.write_file
+  end
+end
 
 ruby_block "edit home s4ds rsyslog.conf" do
   block do
-    rd = Chef::Util::FileEdit.new("/etc/rsyslog.conf")
-    rd.insert_line_if_no_match(/# something./,"local0.*		@logs.papertrailapp.com:39160")
-    rd.write_file
+    rf = Chef::Util::FileEdit.new("/etc/httpd/sites-available/default")
+    rf.search_file_delete_line(/CustomLog/)
+    rf.write_file
   end
+end
+
+bash "insert_line" do
+  user "root"
+  code <<-EOS
+  echo "CustomLog '|/usr/bin/logger -p local1.info -t HTTP'" >> /home/s4ds/Cheftest.conf
+  EOS
+end
+#reiniciar servicio
+service "rsyslog" do
+  action :restart
 end
 
 CustomLog /var/log/httpd/access.log combined
